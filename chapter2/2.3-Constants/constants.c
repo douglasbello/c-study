8930124edb796bc8aa57138d63625f0f41caba6d
douglasbello <douglasbelloalv1@outlook.com>
2023-05-15 17:25:17 -0300

    2.3-Constants

---
+++ chapter2/2.3-Constants/constants.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    const char name[] = "DataFlair"; /* string constant */
+    const long my_long_constant = 100L;
+    const int my_int_constant = 100;
+    const char my_char_constant = 'A';
+    printf("The constant name is: %s\n", name);
+}
